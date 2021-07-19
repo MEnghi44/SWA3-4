e9a44a9f640be91b6b902342c513299cf6793a2d
MEnghi44 <69116659+MEnghi44@users.noreply.github.com>
2021-07-19 15:43:06 +0700

    Initial commit

---
+++ src/com/swa/oop/CarEngine.java
@@ -0,0 +1,16 @@
+package com.swa.oop;
+
+public class CarEngine {
+	
+	public void startEngine(){
+
+        System.out.println("The car engine has Started.");
+
+    }
+
+    public void stopEngine(){
+
+        System.out.println("The car engine has Stopped.");
+
+    }
+}
---
+++ src/com/swa/oop/Main.java
@@ -0,0 +1,18 @@
+package com.swa.oop;
+
+public class Main {
+
+	 public static void main(String[] args) {   
+
+	        Honda HondaJazz = new Honda();
+
+	        HondaJazz.setColour("Black");
+
+	        HondaJazz.setMaxSpeed(160);
+
+	        HondaJazz.carDetails();
+
+	        HondaJazz.HondaStart();
+
+	    }
+}
